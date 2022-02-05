848dc2e9b2f70fbf232d02da6fa0c8645f26692a
Sambath <57914899+sambath393@users.noreply.github.com>
2022-02-05 20:51:53 +0700

    8:51PM

--- src/t18n/translate.js
+++ src/t18n/translate.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-const en = require("./data/en.json")
+const en = require("./resources/en.json")
 
 i18n.use(initReactI18next) // passes i18n down to react-i18next
     .init({
